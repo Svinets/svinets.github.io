c3036426e3b6a759365dd1f994e6f208df9fd7b1
Scott Chao <svinets@gmail.com>
2017-08-17 23:02:58 -0700

    Add map to hold gallery images

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+const urls = ['1', '2', '3', '4', '5'];
+
 const Gallery = (props) => (
   <div>
-    <h1>
-      Gallery!
-    </h1>
+    <h1>Gallery!</h1>
+    {
+      urls.map((url, index) => <span key={index}>{url}</span>)
+    }
   </div>
 );
 
