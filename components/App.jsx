46cf19fd150791f0ecc51aad1499ca305f71ff37
Scott Chao <svinets@gmail.com>
2017-08-17 18:35:57 -0700

    Set up router and import components to app

--- components/App.jsx
+++ components/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { HashRouter as Router, browserHistory, Link, Route, Switch, Redirect } from 'react-router-dom';
+import Profile from './Profile.jsx';
+import Gallery from './Gallery.jsx';
+import Portfolio from './Portfolio.jsx';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+      <Router>
+        <div>
+          <Link to="/"><button>Home</button></Link>
+          <Link to={{pathname: '/gallery'}}><button>Gallery</button></Link>
+          <Link to={{pathname: '/portfolio'}}><button>Portfolio</button></Link>
+          <Switch>
+            <Route exact path="/" component={Profile} />
+            <Route exact path="/gallery" component={Gallery} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
