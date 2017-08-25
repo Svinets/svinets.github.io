296bd1c5db1e8276976e881479c3af1dc523cb9d
Scott Chao <svinets@gmail.com>
2017-08-25 16:04:01 -0700

    Add profile text to Profile.jsx

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import style from '../style.css';
 
 const Profile = (props) => (
   <div>
     <h1>
       Hello!
     </h1>
+    <p>
+      Software engineer specializing in web development. I'm interested in building quick, efficient, and modular client-<br/>
+      side environments to provide the best possible UX. I approach feature implementation as an iterative process, and<br/>
+      my attention to detail and discipline for critiquing visuals require me to maintain my work to a high standard.<br/>
+      <br/>
+      Experienced with front-end frameworks - Angular, React/Redux, with strong fundamentals in HTML and CSS, and<br/>
+      server-side technologies including Node/Express, MySQL, PostgreSQL, and MongoDB databases with accompanying<br/>
+      ODM/ORMs.
+    </p>
   </div>
 );
 
